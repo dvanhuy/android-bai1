2902d5e7ffdac09b024f38fc887d4bf996b804c3
unknown <huydinhvan13@gmail.com>
2022-08-29 14:36:10 +0700

    xong

--- app/src/main/java/com/example/baitap1/manhinh2.java
+++ app/src/main/java/com/example/baitap1/manhinh2.java
@@ -2,16 +2,28 @@ package com.example.baitap1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.TextView;
 
 public class manhinh2 extends AppCompatActivity {
-
+    TextView back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_manhinh2);
+        back= findViewById(R.id.backmain);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent main1 = new Intent(manhinh2.this,MainActivity.class);
+//                startActivity(main1);
+                finish();
+            }
+        });
     }
 }
